51171f8de3411b930651a5372de8ff3601e0461d
Matthew Lansangan <lansanmatt@tamu.edu>
2025-02-26 04:07:33 +0000

    changes to rspec for Google Oauth

--- spec/requests/change_logs_spec.rb
+++ spec/requests/change_logs_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "ChangeLogs", type: :request do
+  before do
+    # Mock authentication by bypassing the authenticate_user! method
+    allow(controller).to receive(:authenticate_user!).and_return(true)
+  end
   describe "GET /index" do
     it "returns http success" do
       get change_logs_path
