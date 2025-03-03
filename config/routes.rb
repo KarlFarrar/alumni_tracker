3dfb215220bc0a90e1dda5dc7041df5601958a0b
Matthew Lansangan <lansanmatt@tamu.edu>
2025-03-03 02:54:58 +0000

    redone google Oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root 'alumni#index'
+  devise_for :gmails, controllers: { omniauth_callbacks:'gmails/omniauth_callbacks' }
+  devise_scope :gmail do
+    get 'gmails/sign_in', to: 'gmails/sessions#new', as: :new_gmail_session
+    get 'gmails/sign_out', to: 'gmails/sessions#destroy', as: :destroy_gmail_session
+  end
   resources :alumni
   resources :change_logs, only: [:index]
-  root 'alumni#index'
 end
---
+++ db/migrate/20250303012835_devise_create_gmails.rb
@@ -0,0 +1,17 @@
+
+
+class DeviseCreateGmails < ActiveRecord::Migration[8.0]
+  def change
+    create_table :gmails do |t|
+      t.string :email,              null: false, default: ""
+      t.string :full_name
+      t.string :uid
+      t.string :avatar_url
+      remove_column :gmails, :full__name
+
+      t.timestamps null: false
+    end
+
+    add_index :gmails, :email,                unique: true
+  end
+end
