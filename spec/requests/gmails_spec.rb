80d58af531743d324535e462a07b34aaff51f57e
Matthew Lansangan <lansanmatt@tamu.edu>
2025-03-04 19:23:38 +0000

    Google Oauth rspec test9

--- spec/requests/gmails_spec.rb
+++ spec/requests/gmails_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Gmails::OmniauthCallbacks", type: :controller do
+RSpec.describe "Gmails::OmniauthCallbacks", type: :system do
   before do 
     driven_by(:rack_test)
 
