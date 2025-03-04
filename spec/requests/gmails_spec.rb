f210803140763c3e8e171897cd2d2af7ae4a4afa
Matthew Lansangan <lansanmatt@tamu.edu>
2025-03-04 17:14:20 +0000

    Google Oauth rspec test6

--- spec/requests/gmails_spec.rb
+++ spec/requests/gmails_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe "Gmails::OmniauthCallbacks", type: :controller do
   describe "Google OAuth2 login" do
     context "when the user exists" do
       it "logs in and redirects to the root path" do
-        post user_google_oauth2_omniauth_callback_path
+        post gmail_google_oauth2_omniauth_callback_path
         expect(controller.current_gmail).to eq(gmail_user)
         expect(response).to redirect_to(root_path)
         expect(flash[:success]).to eq(I18n.t('devise.omniauth_callbacks.success', kind: 'Google'))
@@ -33,7 +33,7 @@ RSpec.describe "Gmails::OmniauthCallbacks", type: :controller do
       end
 
       it "redirects to the sign-up page" do
-        post user_google_oauth2_omniauth_callback_path
+        post gmail_google_oauth2_omniauth_callback_path
         expect(response).to redirect_to(choose_role_registration_path)
         expect(flash[:alert]).to eq("No account found. Please create an account")
       end
