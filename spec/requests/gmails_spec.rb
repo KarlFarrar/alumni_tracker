688595375d7e12876ebd3e177744dd2d3f7f6641
Matthew Lansangan <lansanmatt@tamu.edu>
2025-03-04 19:43:45 +0000

    Google Oauth rspec test13

--- spec/requests/gmails_spec.rb
+++ spec/requests/gmails_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe "Gmails::OmniauthCallbacks", type: :system do
     driven_by(:rack_test)
 
     OmniAuth.config.test_mode = true
+  end 
 
-    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+  context "when the user exists" do 
+    before do
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
       provider: 'google_oauth2',
       uid: '123456',
       info: {
@@ -26,11 +29,35 @@ RSpec.describe "Gmails::OmniauthCallbacks", type: :system do
       uid: '123456',
       avatar_url: 'https://example.com/avatar.jpg'
     )
-  end
+    end
 
-  it 'saves & goes to root node' do
+    it 'saves & goes to root node' do
     visit '/gmails/auth/google_oauth2/callback'
 
     expect(page).to have_current_path(root_path)
-  end  
+    end  
+  end 
+
+  context "When user does not exist" do
+    before do
+      OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '654321',
+      info: {
+        email: 'newuser@example.com',
+        name: 'New user',
+        image: 'https://example.com/newavatar.jpg'
+      },
+      credentials: {
+        token: 'mock_token',
+        expires_at: Time.now + 1.week
+      }
+    })
+    end 
+
+    it 'redirects to redirects to choose_role_registration_path' do
+      visit '/gmails/auth/google_oauth2/callback'
+      expect(page).to have_current_path(choose_role_registration_path)
+    end  
+  end
 end
