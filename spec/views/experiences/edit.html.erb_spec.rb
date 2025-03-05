7ff73ef42e0cce9edbdf2bf52422f474074cf079
KarlFarrar <89283702+KarlFarrar@users.noreply.github.com>
2025-03-05 11:43:50 -0600

    Another Change

--- spec/views/experiences/edit.html.erb_spec.rb
+++ spec/views/experiences/edit.html.erb_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "experiences/edit", type: :view do
   before do
-    assign(:experience, Experience.create!(title: "MyString", experience_type: "Award"))
+    assign(:experience, Experience.create!(title: "MyString", experience_type: "Award")) 
   end
 
   it "renders the edit experience form" do
     render
 
-    assert_select "form[action=?][method=?]", experience_path(@experience), "post" do
+    assert_select "form[action=?][method=?]", experience_path(assigns(:experience)), "post" do
       assert_select "input[name=?]", "experience[title]"
       assert_select "input[name=?]", "experience[experience_type]"
     end
