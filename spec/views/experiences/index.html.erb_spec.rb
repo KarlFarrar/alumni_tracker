3f8dcc20905c0b7bb32d8522e82e4e3f7bfe26b2
KarlFarrar <89283702+KarlFarrar@users.noreply.github.com>
2025-03-05 11:34:57 -0600

    Rspec updates

--- spec/views/experiences/index.html.erb_spec.rb
+++ spec/views/experiences/index.html.erb_spec.rb
@@ -1,23 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "experiences/index", type: :view do
-  before(:each) do
+  before do
     assign(:experiences, [
-      Experience.create!(
-        title: "Title",
-        type: "Type",
-        date_interval: "Date Interval",
-        description: "MyText",
-        recepient_uin: 2
-      )
+      Experience.create!(title: "Title1", experience_type: "Certification"),
+      Experience.create!(title: "Title2", experience_type: "Award")
     ])
   end
 
   it "renders a list of experiences" do
     render
-    cell_selector = 'div>p'
-    assert_select cell_selector, text: Regexp.new("Title".to_s)
-    assert_select cell_selector, text: Regexp.new("Type".to_s)
-
+    expect(rendered).to have_text("Title1")
+    expect(rendered).to have_text("Title2")
   end
 end
--- spec/views/experiences/new.html.erb_spec.rb
+++ spec/views/experiences/new.html.erb_spec.rb
@@ -1,23 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "experiences/new", type: :view do
-  before(:each) do
-    assign(:experience, Experience.new(
-      title: "MyString",
-      type: "Award",
-
-    ))
+  before do
+    assign(:experience, Experience.new(title: "MyString", experience_type: "Award"))
   end
 
   it "renders new experience form" do
     render
 
     assert_select "form[action=?][method=?]", experiences_path, "post" do
-
       assert_select "input[name=?]", "experience[title]"
-
-      assert_select "input[name=?]", "experience[type]"
-
+      assert_select "input[name=?]", "experience[experience_type]"
     end
   end
 end
