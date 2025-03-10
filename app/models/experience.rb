948430af8d6c78682da7527f2c9fe3c2a1a58fc4
KarlFarrar <89283702+KarlFarrar@users.noreply.github.com>
2025-03-10 13:21:19 -0500

    I found were it was persisting

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -6,11 +6,5 @@ class Experience < ApplicationRecord
   validates :experience_type, presence: true
   
   belongs_to :alumnus, foreign_key: "recepient_uin", primary_key: "uin", optional: true
-  
-  validates :title, presence: true
-  validates :experience_type, presence: true  # Updated from 'type'
-  validates :date_interval, presence: true
-  validates :description, presence: true
-  validates :recepient_uin, presence: true, numericality: { only_integer: true }
 end
   
