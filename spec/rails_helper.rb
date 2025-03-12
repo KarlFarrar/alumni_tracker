5e78b63a2d5e77e9efaf8da2135917011a17443b
KarlFarrar <89283702+KarlFarrar@users.noreply.github.com>
2025-03-12 15:50:23 -0500

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,4 +71,13 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::IntegrationHelpers, type: :request
+
+  # Shoulda Matchers Configuration
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
 end
