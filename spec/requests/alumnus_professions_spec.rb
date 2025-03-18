29b59c51cf7104bee6c9eb1d70cdf7576b4f9e76
ShreyasK14 <ShreyasK2014@gmail.com>
2025-03-18 12:10:58 -0500

    part 8

--- spec/requests/alumnus_professions_spec.rb
+++ spec/requests/alumnus_professions_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe "AlumnusProfessions", type: :request do
     # Skip authentication in tests if needed
     allow_any_instance_of(ApplicationController).to receive(:authenticate_gmail!).and_return(true)
   end
-  let(:alumnus) { Alumnus.create!(uin: "123456789", email: "test@example.com") }
+  let(:user) { User.create!(first_name: "test_first", last_name: "test_last", middle_initial: "a", uin: "123456789") }
+  let(:alumnus) { Alumnus.create!(uin: "123456789", email: "test@example.com", user: user) }
   let(:profession) { Profession.create!(title: "Engineer") }
   let!(:alumnus_profession) { AlumnusProfession.create!(alumnus: alumnus, profession: profession, field: "Software") }
 
