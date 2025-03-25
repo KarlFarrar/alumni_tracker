3dc0409c6c8ed9d5534aa932e9791d91b72158f8
Matthew Lansangan <lansanmatt@tamu.edu>
2025-03-25 19:54:01 +0000

    Profile Creation v.8

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_scope :gmail do
     get 'gmails/sign_in', to: 'gmails/sessions#new', as: :new_gmail_session
     get 'gmails/sign_out', to: 'gmails/sessions#destroy', as: :destroy_gmail_session
-    get 'gmails/sign_up', to: 'gmails/registrations#new', as: :new_gmail_registration
     get 'gmails/choose_role', to: 'gmails/registrations#choose_role', as: :choose_role_registration
     post 'gmails', to: 'gmails/registrations#create', as: :create_gmail
   end
