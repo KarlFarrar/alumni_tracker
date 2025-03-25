d345ac650c350f97b93f8e3e544ba82eb7f5e849
Matthew Lansangan <lansanmatt@tamu.edu>
2025-03-25 18:14:26 +0000

    Profile Creation v.2

--- db/migrate/20250314203417_update_gmail.rb
+++ db/migrate/20250314203417_update_gmail.rb
@@ -1,6 +1,6 @@
 class UpdateGmail < ActiveRecord::Migration[8.0]
   def change
-    add_column :gmails, :uin, :integer, null: false
+    add_column :gmails, :uin, :integer, null: false, default: 0
     add_foreign_key :gmails, :users, column: :uin, primary_key: :uin
     add_index :gmails, :uin
     if ActiveRecord::Base.connection.column_exists?(:gmails, :full_name)
