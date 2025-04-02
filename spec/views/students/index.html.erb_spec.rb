da6af9b9c13444ff6f46d88c61063d31c7b67385
KarlFarrar <89283702+KarlFarrar@users.noreply.github.com>
2025-04-02 09:07:00 -0500

    Trying Again

--- spec/views/students/index.html.erb_spec.rb
+++ spec/views/students/index.html.erb_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "students/index", type: :view do
         major: "Major",
         resumelink: "Resumelink",
         email: "Email",
-        phone: "Phone",
+        phone: "(123)-123-1234",
         biography: "Biography",
         user: user2
       )
@@ -39,7 +39,7 @@ RSpec.describe "students/index", type: :view do
     assert_select cell_selector, text: Regexp.new("Major".to_s), count: 2
     assert_select cell_selector, text: Regexp.new("Resumelink".to_s), count: 2
     assert_select cell_selector, text: Regexp.new("Email".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Phone".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("(123)-123-1234".to_s), count: 2
     assert_select cell_selector, text: Regexp.new("Biography".to_s), count: 2
   end
 end
