5a66672f0e25dd1b1ddaa15146c09ab96ab9eab4
Matthew Lansangan <lansanmatt@tamu.edu>
2025-04-04 23:01:06 +0000

    Navbar Rspec v1

---
+++ spec/system/navbar_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+RSpec.describe "Navbar", type: :system do
+  before do
+    driven_by(:rack_test)
+    allow_any_instance_of(ApplicationController).to receive(:authenticate_gmail!).and_return(true)
+  end
+
+  it "redirects to the alumni directory when navbar button is clicked" do
+    visit root_path
+
+    # Simulate clicking the navbar link (adjust the text/id to match your actual link)
+    click_link "Alumni Directory"
+
+    expect(page).to have_current_path(root_path)
+  end
+
+  it "redirects to the student directory when navbar button is clicked" do
+    visit root_path
+
+    # Simulate clicking the navbar link (adjust the text/id to match your actual link)
+    click_link "Student Directory"
+
+    expect(page).to have_current_path(student_directory_path)
+  end
+
+  context "When a user IS AN Admin" do 
+	let(:user) { User.create!(first_name: "test_first", last_name: "test_last", middle_initial: "a", uin: 123456734, isAdmin: true) }
+	  let(:alumnus) {
+	    Alumnus.create!(
+	      uin: 123456734,
+	      cohort_year: 2024,
+	      team_affiliation: "MyString",
+	      profession_title: "MyString",
+	      availability: false,
+	      email: "MyString",
+	      phone_number: "(682)-472-8670",
+	      LinkedIn: "MyString",
+	      user: user
+	    )
+	  }
+  	it "should show the Admin Access button" do 
+
+		  visit root_path
+
+		  expect(page).to have_button("Admin View")
+  	end
+
+  	it "redirects to the admin page when navbar button is clicked" do
+ 		visit root_path
+ 		click_link "Admin View"
+
+    	expect(page).to have_current_path(admin_dashboard_path)
+  	end
+
+
+  end
+
+  context "When a user IS AN Admin" do 
+  	it "should show the Admin Access button" do 
+  		  let(:user) { User.create!(first_name: "test_first", last_name: "test_last", middle_initial: "a", uin: 123456734, isAdmin: false) }
+		  let(:alumnus) {
+		    Alumnus.create!(
+		      uin: 123456734,
+		      cohort_year: 2024,
+		      team_affiliation: "MyString",
+		      profession_title: "MyString",
+		      availability: false,
+		      email: "MyString",
+		      phone_number: "(682)-472-8670",
+		      LinkedIn: "MyString",
+		      user: user
+		    )
+		  }
+
+		  visit root_path
+
+		  expect(page).not_to have_button("Admin View")
+  	end
+  end
+end
