15dcfa1520764db341e78515497d9edf9f805004
Matthew Lansangan <lansanmatt@tamu.edu>
2025-04-04 23:59:17 +0000

    Navbar Rsepc v5.1

--- spec/system/navbar_spec.rb
+++ spec/system/navbar_spec.rb
@@ -34,13 +34,13 @@ RSpec.describe "Navbar", type: :system do
 
     it "redirects to the alumni directory when navbar button is clicked" do
 	    visit root_path
-	    click_link "Alumni Directory"
+	    click_button "Alumni Directory"
 	    expect(page).to have_current_path(root_path)
 	end
 
 	it "redirects to the student directory when navbar button is clicked" do
 	    visit root_path
-	    click_link "Student Directory"
+	    click_button "Student Directory"
 	    expect(page).to have_current_path(student_directory_path)
 	end
 
@@ -53,7 +53,7 @@ RSpec.describe "Navbar", type: :system do
 
   	it "redirects to the admin page when navbar button is clicked" do
  		visit root_path
- 		click_link "Admin View"
+ 		click_button "Admin View"
 
     	expect(page).to have_current_path(admin_dashboard_path)
   	end
