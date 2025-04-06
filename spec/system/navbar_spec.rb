a7b3e2e45fd60b1b54ae160b11c8d746979d01c9
Matthew Lansangan <lansanmatt@tamu.edu>
2025-04-06 00:03:46 +0000

    Navbar Rsepc v7.0

--- spec/system/navbar_spec.rb
+++ spec/system/navbar_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Navbar", type: :system do
-	#let(:user) { User.create!(first_name: "Is", last_name: "Admin", uin: 555555555, isAdmin: true) }
-	#let(:gmail) { Gmail.create!(email: "test@example.com", user: user, avatar_url: "https://lh3.googleusercontent.com/a/ACg8ocKmuBbupm_fHr6Cj2dthGIeHVbsXLa1jyMDZofvOSmIsN-X2g") }
-	#let(:user2) { User.create!(first_name: "Non", last_name: "Admin", uin: 555555556, isAdmin: false) }
-	#let(:gmail2) { Gmail.create!(email: "test@example.com", user: user2, avatar_url: "https://lh3.googleusercontent.com/a/ACg8ocKmuBbupm_fHr6Cj2dthGIeHVbsXLa1jyMDZofvOSmIsN-X2g") }
-
 
   before do
     driven_by(:rack_test)
@@ -36,18 +31,18 @@ RSpec.describe "Navbar", type: :system do
     end
 
     it "redirects to the alumni directory when navbar button is clicked" do
-	    click_link "Alumni Directory"
+	    click_button "Alumni Directory"
 	    expect(page).to have_current_path(root_path)
 	end
 
 	it "redirects to the student directory when navbar button is clicked" do
-	    click_link "Student Directory"
+	    click_button "Student Directory"
 	    expect(page).to have_current_path(student_directory_path)
 	end
 
   	it "redirects to the admin page when navbar button is clicked" do
  		visit root_path
- 		click_on "Admin View"
+ 		click_button "Admin View"
 
     	expect(page).to have_current_path(admin_dashboard_path)
   	end
@@ -72,12 +67,11 @@ RSpec.describe "Navbar", type: :system do
         expires_at: Time.now + 1.week
       }
     })
+      allow_any_instance_of(ApplicationController).to receive(:current_gmail).and_return(gmail)
+      visit root_path
     end
 
 	it "should show the Admin Access button" do 
-
-		  visit root_path
-
 		  expect(page).not_to have_button("Admin View")
   	end
   end
