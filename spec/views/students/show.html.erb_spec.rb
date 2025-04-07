c01084c013b12f08e599d17bf2dba3d392917649
Matthew Lansangan <lansanmatt@tamu.edu>
2025-04-07 04:41:42 +0000

    Student Rspec: added oauth to show_spec

--- spec/views/students/show.html.erb_spec.rb
+++ spec/views/students/show.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "students/show", type: :view do
 
   let(:user) { User.create!(first_name: "test_first", last_name: "test_last", middle_initial: "a", uin: 123456789, isAdmin: false) }
-
+  let(:gmail) { Gmail.create!(email: "Email", user: user, avatar_url: "https://lh3.googleusercontent.com/a/ACg8ocKmuBbupm_fHr6Cj2dthGIeHVbsXLa1jyMDZofvOSmIsN-X2g") }
 
   before(:each) do
     assign(:student, Student.create!(
@@ -16,6 +16,20 @@ RSpec.describe "students/show", type: :view do
       linkedin: "LinkedIn",
       user: user
     ))
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: 'google_oauth2',
+      uid: '123456',
+      info: {
+        email: 'Email',
+        name: 'Test user',
+        image: 'https://lh3.googleusercontent.com/a/ACg8ocKmuBbupm_fHr6Cj2dthGIeHVbsXLa1jyMDZofvOSmIsN-X2g'
+      },
+      credentials: {
+        token: 'mock_token',
+        expires_at: Time.now + 1.week
+      }
+    })
+      allow_any_instance_of(ApplicationController).to receive(:current_gmail).and_return(gmail)
   end
 
   it "renders attributes in <p>" do
