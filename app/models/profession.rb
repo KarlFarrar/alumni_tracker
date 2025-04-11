cc43e5fddc16cc3383c743b30d98f77aaf8dd139
KarlFarrar <89283702+KarlFarrar@users.noreply.github.com>
2025-04-11 13:19:25 -0500

    Update profession.rb

--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,6 +1,6 @@
 class Profession < ApplicationRecord
     include Loggable
-    has_many :alumnus_professions
+    has_many :alumnus_professions, dependent: :destroy
     has_many :alumni, through: :alumnus_professions
 
     validates :title, presence: true, uniqueness: { case_sensitive: false }
